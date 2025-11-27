f0f575066e29cce072b92d3127774e58b48254f0
Laxmanpanwar <151760962+lppanwar@users.noreply.github.com>
2025-11-27 13:46:57 +0530

    change a buttton

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -11,7 +11,7 @@ const Banner = () => {
           <p >Book Appointment</p>
           <p className='mt-4'>With 100+ Trusted Doctors</p>
         </div>
-        <button onClick={() => { Navigate('/login'); scrollTo(0, 0) }} className='bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all'>Create Button</button>
+        <button onClick={() => { Navigate('/login'); scrollTo(0, 0) }} className='bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all'>Create Account</button>
       </div>
 
       {/* /* right div */}
